82297007152f990fd99525d2def84cbf88a87b9c
BenBonno <benoit.bonnoron@gmail.com>
2023-01-11 16:36:28 +0100

    authFix

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 const cors = require("cors");
 const router = require("./router");
-const verifyToken = require("../auth");
+const { verifyToken } = require("../auth");
 
 const app = express();
 
