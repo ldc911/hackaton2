bc4afa039af12ebad6f456fb269aba14734e3840
BenBonno <benoit.bonnoron@gmail.com>
2023-01-11 23:25:04 +0100

    fixForPull

--- backend/src/private.routes.js
+++ backend/src/private.routes.js
@@ -2,6 +2,11 @@ const express = require("express");
 
 const router = express.Router();
 const vehicleControllers = require("./controllers/vehicleControllers");
+const userControllers = require("./controllers/userControllers");
 
+//cars routes
 router.get("/vehicles", vehicleControllers.getAllVehicles);
+
+//users routes
+router.delete("/user/:id", userControllers.deleteUser);
 module.exports = router;
