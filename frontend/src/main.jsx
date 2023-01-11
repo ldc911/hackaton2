c968d15d0abaeef8644b3c2bca272c9f2dbf123a
lucas fasilleau <lucasfaz@hotmail.fr>
2023-01-11 16:56:02 +0100

    ajout de la page login et register en brute

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,15 @@
+// import Login from "@pages/Login";
+import Register from "@pages/Register";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import App from "./App";
+// import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <Login /> */}
+    <Register />
   </React.StrictMode>
 );
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+function Login() {
+  const [password, setPassword] = useState("");
+  const [email, setemail] = useState("");
+
+  return (
+    <div className="bg-[#391397]">
+      <form>
+        <div className="mb-6">
+          <label
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Adresse email
+          </label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setemail(e.target.value)}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            htmlFor="password"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Mot de passe
+          </label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </div>
+        <div className="text-white">Vous n'avez pas de compte ?</div>
+        <button
+          type="submit"
+          className="text-white mt-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mb-4"
+        >
+          SE CONNECTER
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
