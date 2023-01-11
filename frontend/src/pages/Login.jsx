3e43f353cae142eb7236afc5a0af1d684fc72036
Jesuisleon <frederic-versavel_student2022@wilder.school>
2023-01-11 17:42:46 +0100

    fix css

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -5,8 +5,8 @@ function Login() {
   const [email, setemail] = useState("");
 
   return (
-    <div className="bg-[#391397] width content">
-      <form>
+    <div className="width content">
+      <form className="px-10 pt-10">
         <div className="mb-6">
           <label
             htmlFor="email"
@@ -37,7 +37,7 @@ function Login() {
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           />
         </div>
-        <div className="text-white">Vous n'avez pas de compte ?</div>
+        <div>Vous n'avez pas de compte ?</div>
         <button
           type="submit"
           className="text-white mt-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mb-4"
