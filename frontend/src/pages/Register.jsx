b23d2bb207c41c31daa336fb7e65d1a23ccf8c9f
lucas fasilleau <lucasfaz@hotmail.fr>
2023-01-11 17:00:11 +0100

    fix bug

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -145,7 +145,7 @@ function Register() {
             id="floating_repeat_password"
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
-            className="block py-5 px-0  text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            className="block py-2.5 px-2  text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
             required
           />
