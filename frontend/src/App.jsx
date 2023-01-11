fbc4299ff193291b827b44368254041450bacee7
Jesuisleon <frederic-versavel_student2022@wilder.school>
2023-01-11 17:06:50 +0100

    commit before merge with dev

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="h-screen w-screen flex flex-col items-center">
       <Router>
-      <Header />
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/rent" element={<Rent />} />
