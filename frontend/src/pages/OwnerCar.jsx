27fb201a9a7e9d731fb5941a9cc46676bdbbc094
lucas fasilleau <lucasfaz@hotmail.fr>
2023-01-12 23:57:08 +0100

    possibilite de supprimer une voiture sur la page fleet

--- frontend/src/pages/OwnerCar.jsx
+++ frontend/src/pages/OwnerCar.jsx
@@ -141,13 +141,13 @@ export default function OwnerCar() {
             <div>
               <div className="-mt-px flex divide-x divide-gray-200">
                 <div className="-ml-px w-0 flex-1 flex">
-                  <a
-                    href={`tel:${person.telephone}`}
+                  <button
+                    type="button"
                     className="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500"
                     onClick={() => handleDelete(index)}
                   >
                     <span className="ml-3">Supprimer</span>
-                  </a>
+                  </button>
                 </div>
               </div>
             </div>
