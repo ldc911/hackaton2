8e1945d09007f8f8a8f684c9ec1b6b3e00c22f57
BenBonno <benoit.bonnoron@gmail.com>
2023-01-12 17:16:23 +0100

    fuckLesCookies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable import/no-unresolved */
 import "./setup.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Cookies from "universal-cookie";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Login from "./pages/Login";
@@ -10,28 +9,21 @@ import Home from "./pages/Home";
 import Rent from "./pages/Rent";
 import ContactUs from "./pages/ContactUs";
 import CarDetails from "./pages/CarDetails";
-import DataContext from "./contexts/DataContext";
 
 function App() {
-  let token = "";
-  const cookies = new Cookies();
-  token = cookies.cookies?.token;
-
   return (
     <div className="h-screen w-screen flex flex-col items-center">
       <Router>
         <Header />
-        <DataContext.Provider value={token}>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/" element={<Home />} />
-            <Route path="/rent" element={<Rent />} />
-            <Route path="/cardetails/:id" element={<CarDetails />} />
-            <Route path="/contact" element={<ContactUs />} />
-            <Route path="/car/:id" element={<CarDetails />} />
-          </Routes>
-        </DataContext.Provider>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/rent" element={<Rent />} />
+          <Route path="/cardetails/:id" element={<CarDetails />} />
+          <Route path="/contact" element={<ContactUs />} />
+          <Route path="/car/:id" element={<CarDetails />} />
+        </Routes>
         <Footer />
       </Router>
     </div>
